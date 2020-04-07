06c6cb6fce1968829e097cc140b70f83844501d0
TheSwerik <derswerik@gmail.com>
2020-04-07 14:51:36 +0200

    add UI Stuff

--- Frontend/src/MainWindow.xaml.cs
+++ Frontend/src/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
 
 namespace Frontend
 {
@@ -7,6 +8,36 @@ namespace Frontend
         public MainWindow()
         {
             InitializeComponent();
+            ThreadRadioThread.IsChecked = true;
+        }
+
+        private void StartButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            Console.WriteLine("Start");
+        }
+
+        private void CheckBox_OnChecked(object sender, RoutedEventArgs e)
+        {
+            MaxNumberLabel.Visibility = Visibility.Hidden;
+            MaxNumberBox.Visibility = Visibility.Hidden;
+        }
+
+        private void CheckBox_OnUnchecked(object sender, RoutedEventArgs e)
+        {
+            MaxNumberLabel.Visibility = Visibility.Visible;
+            MaxNumberBox.Visibility = Visibility.Visible;
+        }
+
+        private void ThreadRadioThread_OnChecked(object sender, RoutedEventArgs e)
+        {
+            ThreadLabel.Visibility = Visibility.Visible;
+            ThreadBox.Visibility = Visibility.Visible;
+        }
+
+        private void ThreadRadioTask_OnChecked(object sender, RoutedEventArgs e)
+        {
+            ThreadLabel.Visibility = Visibility.Hidden;
+            ThreadBox.Visibility = Visibility.Hidden;
         }
     }
 }
